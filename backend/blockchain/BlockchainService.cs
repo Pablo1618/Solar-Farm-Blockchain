74ac367e0fef095c7d93eca0e8e32c6b32fdf447
Bartosz Sontowski <bartek.sontowski@gmail.com>
2025-12-01 20:42:04 +0100

    Uploaded contract to blockchain added some transfer features

--- backend/blockchain/BlockchainService.cs
+++ backend/blockchain/BlockchainService.cs
@@ -1,11 +1,17 @@
 ﻿using Backend;
+using ContractCompile.Contracts.SolarToken;
+using ContractCompile.Contracts.SolarToken.ContractDefinition;
 using Nethereum;
 using Nethereum.Hex.HexConvertors.Extensions;
+using Nethereum.Model;
 using Nethereum.Web3;
+using Nethereum.Web3.Accounts;
 using System;
 using System.CodeDom.Compiler;
 using System.Net;
 using System.Net.NetworkInformation;
+using System.Security.Cryptography;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace SolarFarmBackend.blockchain;
@@ -14,6 +20,10 @@ class BlockchainService
 {
     private Web3 _web3;
     private readonly ILogger<BlockchainService> _logger;
+    private string _mainAccountPrivateKey = "0xc2af33e1212b9d16de61540cd1133912ef3bc9b391421b029a60256d1637f75b";
+    private string _contractAdress = "0x5f82cba1e72ac9dd9830184cf7f21128a00c64fd"; //Adres tokena ERC20
+    private string _rpcAdress = "https://1rpc.io/sepolia";
+    private SolarTokenService _solarTokenService;
     public BlockchainService(ILogger<BlockchainService> logger)
     {
         _logger = logger;
@@ -21,26 +31,67 @@ class BlockchainService
         var account = new Nethereum.Web3.Accounts.Account(privateKey, 11155111);
         _web3 = new Web3(account, "https://1rpc.io/sepolia");
 
-        _logger.LogInformation(account.Address.ToString());
+        _solarTokenService = new SolarTokenService(_web3, _contractAdress);
+        
+        var other_account = StringToAccount("0");
+
+        var receipt = _solarTokenService.TransferRequestAndWaitForReceiptAsync(new TransferFunction { To = other_account.Address, Value = 1 });
+        receipt.Wait();
+        //_solarTokenService.TransferFromRequestAsync(new TransferFromFunction { From = account.Address, To = other_account.Address, Value = 1 }).Wait();
+
+        var balance = _solarTokenService.BalanceOfQueryAsync(new BalanceOfFunction { Account = account.Address});
+        balance.Wait();
+        _logger.LogInformation("Balance: " + balance.Result);
+
+        balance = _solarTokenService.BalanceOfQueryAsync(new BalanceOfFunction { Account = other_account.Address });
+        balance.Wait();
+        _logger.LogInformation("Balance: " + balance.Result);
 
 
-        GenerateAccountsHelper();
+        //var balance = _solarTokenService.BalanceOfQueryAsync(new BalanceOfFunction { Account = account.Address});
+        //balance.Wait();
+        //_logger.LogInformation("Balance: " + balance.Result);
+
+        //var contractAdress = DeployContractAndGetAndGetAdress();
+        //contractAdress.Wait();
+        //_logger.LogInformation("Adress contract: " + contractAdress.Result);
     }
 
+    private async Task<string> DeployContractAndGetAndGetAdress()
+        {
+        SolarTokenDeployment deployment = new SolarTokenDeployment();
+        deployment.InitialSupply = 1000000000;
+        SolarTokenService service = await ContractCompile.Contracts.SolarToken.SolarTokenService.DeployContractAndGetServiceAsync(_web3, deployment);
+        return service.ContractAddress;
+    }
+
+    private Nethereum.Web3.Accounts.Account StringToAccount(string Name)
+    {
+        SHA256 mySHA256 = SHA256.Create();
+        var namePostfix = "89f4569e";
+        byte[] valueToHash = Encoding.ASCII.GetBytes(Name + namePostfix);
+        byte[] privateKey = mySHA256.ComputeHash(valueToHash);
+        var key = new Nethereum.Signer.EthECKey(privateKey, true);
+        var account = new Nethereum.Web3.Accounts.Account(key);
+        _logger.LogInformation("Account Address: " + account.Address.ToString() + " Private Key: " + account.PrivateKey.ToString());
+        return account;
+    }
     private async void GenerateAccountsHelper()
     {
         //var ecKey = Nethereum.Signer.EthECKey.GenerateKey();
         //var privateKey = ecKey.GetPrivateKeyAsBytes().ToHex();
-        //
         //var account = new Nethereum.Web3.Accounts.Account(privateKey);
-        //_web3.Eth.Accounts.Creat
         //_logger.LogInformation("Account Address: " + account.Address.ToString() + " Private Key: " + account.PrivateKey.ToString());
+        //_web3.Eth.Accounts.Creat
+
 
         //var output = await _web3.Eth.Accounts.SendRequestAsync();
         //_logger.LogInformation(output.ToString());
-        var balance = await _web3.Eth.GetBalance.SendRequestAsync("0x7728Bb890cEA31185Ce81f1cFAd6391e22A731af");
-        var etherAmount = Web3.Convert.FromWei(balance.Value);
-        _logger.LogInformation($"Balance in Ether: {etherAmount}");
+
+        //Check balance
+        //var balance = await _web3.Eth.GetBalance.SendRequestAsync("0x7728Bb890cEA31185Ce81f1cFAd6391e22A731af");
+        //var etherAmount = Web3.Convert.FromWei(balance.Value);
+        //_logger.LogInformation($"Balance in Ether: {etherAmount}");
     }
     static async Task GetAccountBalance()
     {
